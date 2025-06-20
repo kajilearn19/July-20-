2d20c68076335bb3334c273229d80f53df880aba
Kajaanan <kajilearn@gmail.com>
2025-06-20 21:32:56 +0530

    Update Kada.java

--- Kada.java
+++ Kada.java
@@ -1,5 +1,10 @@
 public class Kada{
   public static void main(String args[]){
     System.out.println("hi daa");
+    for (int i=0;i<=5;i++){
+      for (int j=0;j<=i;j++){
+      System.out.print("*");
+      }
+    }
   }
 }
